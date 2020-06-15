cce1f6978fbad3d80c2b2d660d3a8f946fca4028
Joao Guerreiro <joaofilipedg@gmail.com>
2020-06-15 10:44:10 +0100

    Added timer to terminate reservations

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_app.src import sql_connection as sql
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from flask_apscheduler import APScheduler
+from datetime import datetime, timedelta
+
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder="templates/static")
-
-db = sql.db_connect("sqlite_db/res_alloc.db")
 
 @app.route('/')
 @app.route('/login')
@@ -14,24 +17,27 @@ def hello_world():
 
 @app.route('/reservations')
 def reservations():
-    list_hosts = sql.db_getListHosts(db)
+    list_hosts = sql.db_getListHosts()
     print(list_hosts)
 
-    list_restypes, list_restypes_ids = sql.db_getListResTypes(db)
+    list_restypes, list_restypes_ids = sql.db_getListResTypes()
     print(list_restypes)
     print(list_restypes_ids)
     
-    list_freehosts = sql.db_getFreeHosts(db)
+    list_freehosts = sql.db_getFreeHosts()
     print(list_freehosts)
 
-    list_res = sql.db_getCurrentReservations(db)
+    list_res = sql.db_getCurrentReservations()
     print(list_res)
     return render_template('layouts/reservations.html', hosts=list_hosts, num_res_types=len(list_restypes), res_types=list_restypes, res_type_ids = list_restypes_ids, free_hosts=list_freehosts, curr_res=list_res)
 
 @app.route('/new_reservation', methods=["POST"])
 def new_reservation():
     # if request.method == "POST":
-    list_users = sql.db_getListUsers(db)
+
+
+
+    list_users = sql.db_getListUsers()
 
     new_reservation = {}
     new_reservation["user"] = request.form["username"]
@@ -47,7 +53,19 @@ def new_reservation():
     new_reservation["begin_date"] = datetimes[0]
     new_reservation["end_date"] = datetimes[1]
 
-    sql.db_addNewReservation(db, new_reservation)
+    now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    # print(now)
+    # print(new_reservation["end_date"] )
+    # check if reservation enddate is already in the past
+    if new_reservation["end_date"] <= now:
+        return "RESERVATION END DATE IS ALREADY IN THE PAST ({})!".format(new_reservation["end_date"])
+
+    
+    res_id = sql.db_addNewReservation(new_reservation)
+
+    # print
+    # scheduler.add_job(func=sql.db_timedRemoveReservation, trigger="date", run_date=new_reservation["end_date"], args=[res_id, db], id='j'+str(res_id))
+    scheduler.add_job(func=sql.db_timedRemoveReservation, trigger="date", run_date=new_reservation["end_date"], args=[res_id], id='j'+str(res_id))
     
     return redirect(url_for('reservations'))
 
@@ -71,8 +89,57 @@ def cancel_reservation():
     # print(request.get_json())
     res_id = request.get_json().get("res_id", "")
     print(res_id)
-    sql.db_removeReservation(db, res_id)
+    sql.db_removeReservation(res_id)
     return "OK"
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/joaof/OneDrive/github/joaofilipedg/resourceAllocation/sqlite_db/res_alloc.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new_sqlalchemy_db.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db_alch = SQLAlchemy(app)
+
+# class User(db_alch.Model):
+#     id = db_alch.Column(db_alch.Integer, primary_key=True)
+#     username = db_alch.Column(db_alch.String(80), unique=True, nullable=False)
+#     email = db_alch.Column(db_alch.String(120), unique=True, nullable=False)
+#     def __repr__(self):
+#             return '<User %r>' % self.username
+
+# def show_users():
+#     with db_alch.app.app_context():
+#         print(User.query.all())
+
+def timed_func():
+    print("5 seconds later")
+
+# def sched_addNewRes(sched, new_reservation):
+    
+
+
+class Config(object):
+    # JOBS = [
+    #     {
+    #         'id': 'job1',
+    #         # 'func': show_users,
+    #         'func': timed_func,
+    #         'trigger': 'interval',    
+    #         'replace_existing': True,
+    #         'seconds': 5
+    #     }
+    # ]
+
+    SCHEDULER_JOBSTORES = {
+        'default': SQLAlchemyJobStore(url='sqlite:///flask_context.db')
+    }
+
+    SCHEDULER_API_ENABLED = True
+
+app.config.from_object(Config())
+# db_alch.app = app
+# db_alch.init_app(app)
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- flask_app/src/sql_connection.py
+++ flask_app/src/sql_connection.py
@@ -6,42 +6,44 @@ def db_connect(path_to_db):
     db_conn = sqlite3.connect(path_to_db)
     return db_conn
 
+db = db_connect("sqlite_db/res_alloc.db")
+
 # Query the database
-def db_query(db, query):
+def db_query(query):
     cursor = db.cursor()
     cursor.execute(query)
     return cursor.fetchall()
 
 
-def db_queryFirstColumn(db, query):
-    result = db_query(db, query)
+def db_queryFirstColumn(query):
+    result = db_query(query)
     list_values = [i[0] for i in result]
     return list_values
 
 # Convert the query results output into a list
-def db_listFromQuery(db, query):
-    result = db_query(db, query)
+def db_listFromQuery(query):
+    result = db_query(query)
     list_values = [i[0] for i in result]
     list_ids = [i[1] for i in result]
     return list_values, list_ids
 
 # Get list of users
-def db_getListUsers(db):
+def db_getListUsers():
     query = "SELECT username FROM users ORDER BY username;"
-    return db_queryFirstColumn(db, query)
+    return db_queryFirstColumn(query)
 
 # Get list of hosts
-def db_getListHosts(db):
+def db_getListHosts():
     query = "SELECT hostname FROM hosts ORDER BY hostname;"
-    return db_queryFirstColumn(db, query)
+    return db_queryFirstColumn(query)
 
 # Get list of possible reservation types
-def db_getListResTypes(db):
+def db_getListResTypes():
     query = "SELECT name, id, description FROM reservation_types;"
-    return db_listFromQuery(db, query)
+    return db_listFromQuery(query)
 
 # Get list of free hosts
-def db_getFreeHosts(db):
+def db_getFreeHosts():
     query = "SELECT \
                 hosts.hostname, \
                 COUNT(res.host) as num_reservations \
@@ -51,10 +53,10 @@ def db_getFreeHosts(db):
             GROUP BY hosts.hostname \
             HAVING num_reservations == 0 \
             ORDER BY hosts.hostname;"
-    return db_queryFirstColumn(db, query)
+    return db_queryFirstColumn(query)
 
 # Get list of current reservations
-def db_getCurrentReservations(db):
+def db_getCurrentReservations():
     query = "SELECT \
                 hosts.hostname, \
                 users.username, \
@@ -68,7 +70,7 @@ def db_getCurrentReservations(db):
                 LEFT JOIN users ON users.username = res.user \
                 LEFT JOIN reservation_types as res_t ON res_t.id = res.reservation_type \
             ORDER BY 1,2;"
-    list_res = db_query(db, query)
+    list_res = db_query(query)
 
     list_aux = []
     for res in list_res:
@@ -80,18 +82,19 @@ def db_getCurrentReservations(db):
 
 INSERT_INTO = "INSERT INTO {} VALUES {};"
 RESERVATIONS_TABLE = "reservations(user, host, reservation_type, begin_date, end_date)"
-def db_addNewReservation(db, new_res):
+def db_addNewReservation(new_res):
     new_res_str = "(\"{}\", \"{}\", {}, \"{}\", \"{}\")".format(new_res["user"], new_res["host"], new_res["res_type"], new_res["begin_date"], new_res["end_date"])
     insert = INSERT_INTO.format(RESERVATIONS_TABLE, new_res_str)
     
     print(insert)
     cursor = db.cursor()
     cursor.execute(insert)
+    # print(cursor.lastrowid)
     db.commit()
 
-    return True
+    return cursor.lastrowid
 
-def db_removeReservation(db, res_id):
+def db_removeReservation(res_id):
     delete = "DELETE FROM reservations WHERE id={}".format(res_id)
     
     print(delete)
@@ -99,4 +102,16 @@ def db_removeReservation(db, res_id):
     cursor.execute(delete)
     db.commit()
 
+    return True
+
+def db_timedRemoveReservation(*args):
+    res_id = args[0]
+    print("Time's up! Finishing reservation with id {}".format(res_id))
+    db_temp = db_connect("sqlite_db/res_alloc.db")
+
+    delete = "DELETE FROM reservations WHERE id={}".format(res_id)
+    print(delete)
+    cursor = db_temp.cursor()
+    cursor.execute(delete)
+    db_temp.commit()
     return True
